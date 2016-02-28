0e84f950ec07fa70878de00f17aac07bb8e26824
Franklin Cardenas Fox <francarfox@gmail.com>
2016-02-28 17:34:35 -0300

    Add iOS Project

---
+++ NoahKit/NoahKit/Knight.swift
@@ -0,0 +1,13 @@
+//
+//  Knight.swift
+//  NoahKit
+//
+//  Created by Franklin Fox on 28/2/16.
+//  Copyright © 2016 FoxDev. All rights reserved.
+//
+
+import UIKit
+
+public class Knight: NSObject {
+    public var name : String!
+}
